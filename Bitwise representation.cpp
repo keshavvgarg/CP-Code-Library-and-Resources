8e2b3cb867713e34cf35172bcae704850f873d7b
Keshav Garg <keshavssgarg@gmail.com>
2020-09-23 17:48:10 +0530

    Update and rename Bitwise representation.txt to Bitwise representation.cpp

--- Bitwise representation.cpp
+++ Bitwise representation.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main(){
+
 int x = 127;
 
 for(int i=31;i>=0;i--){
-if((x&(1<<i))!=0) {cout<<"1";}
-else {cout<<"0";}
+  if((x&(1<<i))!=0) { // If ith bit is 1
+   cout<<"1";
+  }
+  else{ // If ith bit is zero
+  cout<<"0";
+  }
 }
 
 return 0;
